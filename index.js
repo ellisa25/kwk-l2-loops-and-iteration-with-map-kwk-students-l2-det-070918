f0b7df365ffb13b28067c8db02fffb367add3899
ellisa25 <ewhill0306@gmail.com>
2018-07-16 17:16:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ function lowerCaseStudentNames (list){
     return driver.toLowerCase();
   });
 }
-function nameToAttributes (list){
-  return list.map(function(driver){
-    return driver.nameToAttributes();
-  })
-}
